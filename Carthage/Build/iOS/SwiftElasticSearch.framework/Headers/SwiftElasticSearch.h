1c4db2327d6f4ef1096262320f3cb8745ae32dea
Harsh Patel <harshaudi27@gmail.com>
2018-11-19 10:45:35 +0530

    Added Cartage support for publishing framework

---
+++ Carthage/Build/iOS/SwiftElasticSearch.framework/Headers/SwiftElasticSearch.h
@@ -0,0 +1,19 @@
+//
+//  SwiftElasticSearch.h
+//  SwiftElasticSearch
+//
+//  Created by Harsh Patel on 04/11/18.
+//  Copyright © 2018 Harsh Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftElasticSearch.
+FOUNDATION_EXPORT double SwiftElasticSearchVersionNumber;
+
+//! Project version string for SwiftElasticSearch.
+FOUNDATION_EXPORT const unsigned char SwiftElasticSearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftElasticSearch/PublicHeader.h>
+
+
